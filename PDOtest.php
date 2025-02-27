dfbe3c9df78fcbf8414ddbddba0ddfdec464a497
VitekPro2005 <viktorshevelev224@gmail.com>
2025-02-28 03:34:41 +0700

    changed some things to be more readable

--- PDOtest.php
+++ PDOtest.php
@@ -39,21 +39,29 @@ $statement = $db->query('CREATE TABLE IF NOT EXISTS `posts` (
 $categories = ['Coding', 'Finance', 'Movies'];
 $statement = $db->prepare("INSERT INTO categories (title) VALUES (:title)");
 foreach ($categories as $category) {
-    $statement->execute([':title' => $category]);
+$statement->execute([':title' => $category]);
 }
 
 //Добавляем категории в массив с помощью цикла foreach, который проходится по каждому элементу
 //и добавляет в таблицу три категории
 
 $posts = [
-	['title' => 'PHP 101', 'content' => 'PHP is easy to learn but hard to master.', 'category_id' => 1],
-	['title' => 'Saving for the future', 'content' => 'How to manage your money to make sure you save enough.', 'category_id' => 2],
-	['title' => 'The Tarantino Effect', 'content' => 'An impact of one director dedicated to his craft.', 'category_id' => 3],
+	['title' => 'PHP 101',
+	'content' => 'PHP is easy to learn but hard to master.',
+	'category_id' => 1],
+	['title' => 'Saving for the future',
+	'content' => 'How to manage your money to make sure you save enough.',
+	'category_id' => 2],
+	['title' => 'The Tarantino Effect',
+	'content' => 'An impact of one director dedicated to his craft.',
+	'category_id' => 3],
 ];
 
 $statement = $db->prepare("INSERT INTO posts (title, content, category_id) VALUES (:title, :content, :category_id)");
 foreach ($posts as $post) {
-	$statement->execute([':title' => $post['title'], ':content' => $post['content'], ':category_id' => $post['category_id']]);
+	$statement->execute([':title' => $post['title'],
+	':content' => $post['content'],
+	':category_id' => $post['category_id']]);
 }
 
 //Создаём массив постов, с помощью цикла foreach проходим по каждому его элементу и заполняем соответствующими значениями
